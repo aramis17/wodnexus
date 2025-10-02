c4e86fa752c7784aa8549298280e0b6f29965713
Omar Aramis Valdez Chio <oaramis17@gmail.com>
2025-10-02 03:03:16 +0000

    Quiero que cambies los colores por unos colores oscuros que sean moderno

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" suppressHydrationWarning>
+    <html lang="es" suppressHydrationWarning className="dark">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
