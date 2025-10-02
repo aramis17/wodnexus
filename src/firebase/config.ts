9f78b123fd8545fab25ce9e83a4ed125e7088a33
Omar Aramis Valdez Chio <oaramis17@gmail.com>
2025-10-02 21:43:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9647848407-a2c61",
+  "appId": "1:643166088911:web:c1aba6d8e31c003917d1ba",
+  "apiKey": "AIzaSyC9p5CGu_YuC5RUA38IDD6ZaRQWtzeo4SM",
+  "authDomain": "studio-9647848407-a2c61.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "643166088911"
+};
